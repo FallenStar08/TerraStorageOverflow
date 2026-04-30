2c3df299d0403f93590453e72841ba28df6939c0
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2026-04-30 04:59:33 +0200

    Patch conditions to allow sinks to count as near water

---
+++ Common/Systems/CraftingConditionSystem.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TerraStorage.Common;
+using TerraStorage.Content.UI.Elements;
+
+namespace TerraStorageOverflow.Common.Systems
+{
+    public class CraftingConditionSystem : ModSystem
+    {
+        public override void Load()
+        {
+            MethodInfo getItemCondition = typeof(ItemSearchHelper).GetMethod("GetItemCondition",
+                BindingFlags.NonPublic | BindingFlags.Static);
+
+            if (getItemCondition != null)
+            {
+                MonoModHooks.Add(getItemCondition, Detour_GetItemCondition);
+            }
+        }
+
+        private delegate CraftingCondition orig_GetItemCondition(int itemType);
+
+        private static CraftingCondition Detour_GetItemCondition(orig_GetItemCondition orig, int itemType)
+        {
+            return IsNearWater(itemType)
+                ? CraftingCondition.NearWater
+                : IsNearLava(itemType) ? CraftingCondition.NearLava : IsNearHoney(itemType) ? CraftingCondition.NearHoney : orig(itemType);
+        }
+
+        private static bool IsNearWater(int itemType)
+        {
+            Item item = new();
+            item.SetDefaults(itemType);
+
+            return item.createTile >= TileID.Dirt && TileID.Sets.CountsAsWaterSource[item.createTile];
+        }
+
+        private static bool IsNearLava(int itemType)
+        {
+            Item item = new();
+            item.SetDefaults(itemType);
+
+            return item.createTile >= TileID.Dirt && TileID.Sets.CountsAsLavaSource[item.createTile];
+        }
+
+        private static bool IsNearHoney(int itemType)
+        {
+            Item item = new();
+            item.SetDefaults(itemType);
+
+            return item.createTile >= TileID.Dirt && TileID.Sets.CountsAsHoneySource[item.createTile];
+        }
+    }
+}
